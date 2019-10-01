9d9e80b2dc6b5eee67d8b022098f04517a610c4e
moonti <tmmoond8@gmail.com>
2019-10-01 23:07:11 +0900

    퍼즐 조각 이동     - throttle 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Puzzle from '../components/Puzzle';
+const puzzle = [
+  0, 1, 2,
+  3, 4, 5,
+  6, 7, 8,
+  9,
+];
 
 const Index = () => (
-  <div>
-    juest
-  </div>
+  <Puzzle img="https://i.imgur.com/7bNfhgP.jpg" puzzle={puzzle} row={3}/>
 );
 
 export default Index;
